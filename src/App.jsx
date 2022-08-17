03b66e85e4f773bd5165eb0d47691f04811995e7
Uriel Olmedo <uriel.olme@gmail.com>
2022-08-17 20:00:22 -0300

    clean project

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Routes, Route } from 'react-router-dom'
 
-function App() {
-  const [count, setCount] = useState(0)
+import NavBar from './components/NavBar/NavBar'
+import Landing from './components/Landing/Landing'
+import Games from './components/Games/Games'
+import GameDetail from './components/GameDetail/GameDetail'
+import Upload from './components/UploadGame/Upload'
+import React from 'react'
+import NotFound404 from './components/NotFound404/NotFound404'
 
+function App () {
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <NavBar />
+
+      <Routes>
+
+        <Route path="/" element={<Landing />} />
+        <Route path="games" element={<Games />} />
+        <Route path="/:id" element={<GameDetail />} />
+        <Route path="/upload" element={<Upload />} />
+        <Route path="*" element={<NotFound404 />} />
+
+      </Routes>
+
     </div>
   )
 }
