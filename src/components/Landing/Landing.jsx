afebed46f1e6166bfc25bab69baa0590844bba98
Uriel Olmedo <uriel.olme@gmail.com>
2022-08-19 19:47:31 -0300

    landing mobile first

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import style from './Landing.module.css'
 
 export default function Landing () {
   return (
-    <div>Landing</div>
+    <main className='container'>
+      <section className={style.heroContainer}>
+
+        <figure className={style.imageContainer}>
+          <img src="/images/hero.svg" alt="Hero image" />
+        </figure>
+
+        <div className={style.heroText}>
+          <h1 className='title-retro'>Welcome!</h1>
+          <p>Aquí encontrarás el listado más completo de videojuegos de todas las plataformas. ¿Buscas información, novedades o si merece la pena comprar algún título en concreto? En esta página encontrarás recopilados todos los juegos que han salido al mercado o que van a salir en un futuro.</p>
+          <Link to='/games'>
+            <button className={style.startBtn}>
+              Start
+            </button>
+          </Link>
+        </div>
+
+      </section>
+
+    </main>
   )
 }
